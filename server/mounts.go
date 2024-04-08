58f6a22e960a0783d0ba71d0930b56d156cac233
Michael (Parker) Parker <parkervcp@gmail.com>
2024-04-08 09:23:35 -0400

    resolve issues with missing user in container
    adds a passwd file mount to resolve issues with missing user. default value is true.
    
    adds a config option to disable the option if wanted
    
    Resolves issues with mariadb and other game servers such as arma

--- server/mounts.go
+++ server/mounts.go
@@ -29,6 +29,16 @@ func (s *Server) Mounts() []environment.Mount {
 		},
 	}
 
+	if config.Get().System.User.Passwd {
+		passwdMount := environment.Mount{
+			Default:  true,
+			Target:   "/etc/passwd",
+			Source:   config.Get().System.User.PasswdFile,
+			ReadOnly: true,
+		}
+
+		m = append(m, passwdMount)
+	}
 	// Also include any of this server's custom mounts when returning them.
 	return append(m, s.customMounts()...)
 }
