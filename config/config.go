3102b2d9f63413ee9f84def7d81e814a25dc385e
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2024-09-14 17:14:16 +0200

    other var names

--- config/config.go
+++ config/config.go
@@ -321,9 +321,10 @@ type Configuration struct {
 	// This is required to have the "Server Mounts" feature work properly.
 	AllowedMounts []string `json:"-" yaml:"allowed_mounts"`
 
-	// Check if you are allowed to mount to a sub dir of /home/container
-	// Default false as this is bad practise but some people want to do this.
-	AllowBaseDirMount bool `default:"false" json:"-" yaml:"AllowBaseDirMount"`
+	// BlockBaseDirMount indicates whether mounting to /home/container is blocked.
+	// If true, mounting to /home/container is blocked.
+	// If false, mounting to /home/container is allowed.
+	BlockBaseDirMount bool `default:"true" json:"-" yaml:"BlockBaseDirMount"`
 
 	// AllowedOrigins is a list of allowed request origins.
 	// The Panel URL is automatically allowed, this is only needed for adding
