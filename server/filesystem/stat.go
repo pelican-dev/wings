e8ef74afedff6b0f519f12c057e60cf103a35399
QuintenQVD0 <josdekurk@gmail.com>
2024-10-06 12:18:54 +0200

    fix: symlinks detecting

--- server/filesystem/stat.go
+++ server/filesystem/stat.go
@@ -38,7 +38,7 @@ func (s *Stat) MarshalJSON() ([]byte, error) {
 		Size:      s.Size(),
 		Directory: s.IsDir(),
 		File:      !s.IsDir(),
-		Symlink:   s.Mode().Perm()&ufs.ModeSymlink != 0,
+		Symlink:   s.Mode().Type()&ufs.ModeSymlink != 0,
 		Mime:      s.Mimetype,
 	})
 }
