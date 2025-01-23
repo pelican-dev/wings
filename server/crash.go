5a06c3a2da789420cb55e6b93fa7c66a6e82f550
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2025-01-23 20:51:15 +0100

    Don't panic just log it

--- server/crash.go
+++ server/crash.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"emperror.dev/errors"
+	"github.com/apex/log"
 
 	"github.com/pelican-dev/wings/config"
 	"github.com/pelican-dev/wings/environment"
@@ -71,12 +72,12 @@ func (s *Server) handleServerCrash() error {
 		return nil
 	}
 
-	// Get the last line from the output before the crash so we can log it
+	// Get the last lines from the output before the crash so we can log it
 	logs, err := s.Environment.Readlog(config.Get().System.CrashActivityLogLines)
 	if err != nil {
-		return errors.Wrap(err, "Faild to get the last line out of the console")
+		log.WithField("server_id", s.ID()).Warn("Faild to get the last lines out of the console for the activity logs")
 	}
-	
+
 	s.PublishConsoleOutputFromDaemon("---------- Detected server process in a crashed state! ----------")
 	s.PublishConsoleOutputFromDaemon(fmt.Sprintf("Exit code: %d", exitCode))
 	s.PublishConsoleOutputFromDaemon(fmt.Sprintf("Out of memory: %t", oomKilled))
