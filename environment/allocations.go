3235175b4ea4e59dc069fe2f8e84bc9af5f3f6d8
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2025-06-26 08:39:43 +0200

    Servers with no allocation (#86)
    * Update packages
    
    * Docker: server with no allocation
    
    * None bracking package updates
    
    * Make SEVRER_IP 127.0.0.1
    
    * Compare to port and not IP
    
    * This is not needed anymore as we use 127.0.0.1:0
    
    * Fix ForceOutgoingIp
    
    * Fix packages conflict

--- environment/allocations.go
+++ environment/allocations.go
@@ -9,6 +9,12 @@ import (
 	"github.com/pelican-dev/wings/config"
 )
 
+
+// DefaultAllocationMapping defines the IP and port mapping for a server
+type DefaultAllocationMapping struct {
+	Ip   string `json:"ip"`
+	Port int    `json:"port"`
+}
 // Defines the allocations available for a given server. When using the Docker environment
 // driver these correspond to mappings for the container that allow external connections.
 type Allocations struct {
@@ -17,13 +23,11 @@ type Allocations struct {
 	// the DefaultMapping's IP. This is important to servers which rely on external
 	// services that check the IP of the server (Source Engine servers, for example).
 	ForceOutgoingIP bool `json:"force_outgoing_ip"`
+	
 	// Defines the default allocation that should be used for this server. This is
 	// what will be used for {SERVER_IP} and {SERVER_PORT} when modifying configuration
 	// files or the startup arguments for a server.
-	DefaultMapping struct {
-		Ip   string `json:"ip"`
-		Port int    `json:"port"`
-	} `json:"default"`
+	DefaultMapping *DefaultAllocationMapping `json:"default"`
 
 	// Mappings contains all the ports that should be assigned to a given server
 	// attached to the IP they correspond to.
