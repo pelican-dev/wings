859d07dd7c4b855b536e01bcc46b8df7b8475b8e
Garbg <83542885+Garbg@users.noreply.github.com>
2025-07-04 17:01:40 -0400

    Convert cmd/configure all the way over to huh

--- cmd/configure.go
+++ cmd/configure.go
@@ -11,8 +11,6 @@ import (
 	"regexp"
 	"time"
 
-	"github.com/AlecAivazis/survey/v2"
-	"github.com/AlecAivazis/survey/v2/terminal"
 	"github.com/charmbracelet/huh"
 	"github.com/goccy/go-json"
 	"github.com/pelican-dev/wings/config"
@@ -64,54 +62,45 @@ func configureCmdRun(cmd *cobra.Command, args []string) {
 	} else if err != nil && !os.IsNotExist(err) {
 		panic(err)
 	}
-
-	var questions []*survey.Question
+	var fields []huh.Field
 	if configureArgs.PanelURL == "" {
-		questions = append(questions, &survey.Question{
-			Name:   "PanelURL",
-			Prompt: &survey.Input{Message: "Panel URL: "},
-			Validate: func(ans interface{}) error {
-				if str, ok := ans.(string); ok {
-					_, err := url.ParseRequestURI(str)
-					return err
-				}
-				return nil
-			},
-		})
+		fields = append(fields, huh.NewInput().
+			Title("Panel URL: ").
+			Validate(func(str string) error {
+				_, err := url.ParseRequestURI(str)
+				return err
+			}).
+			Value(&configureArgs.PanelURL),
+		)
 	}
 
 	if configureArgs.Token == "" {
-		questions = append(questions, &survey.Question{
-			Name:   "Token",
-			Prompt: &survey.Input{Message: "API Token: "},
-			Validate: func(ans interface{}) error {
-				if str, ok := ans.(string); ok {
-					if len(str) == 0 {
-						return fmt.Errorf("please provide a valid authentication token")
-					}
+		fields = append(fields, huh.NewInput().
+			Title("API Token: ").
+			Validate(func(str string) error {
+				if len(str) == 0 {
+					return fmt.Errorf("please provide a valid authentication token")
 				}
 				return nil
-			},
-		})
+			}).
+			Value(&configureArgs.Token),
+		)
 	}
 
 	if configureArgs.Node == "" {
-		questions = append(questions, &survey.Question{
-			Name:   "Node",
-			Prompt: &survey.Input{Message: "Node ID: "},
-			Validate: func(ans interface{}) error {
-				if str, ok := ans.(string); ok {
-					if !nodeIdRegex.Match([]byte(str)) {
-						return fmt.Errorf("please provide a valid authentication token")
-					}
+		fields = append(fields, huh.NewInput().
+			Title("Node ID: ").
+			Validate(func(str string) error {
+				if !nodeIdRegex.Match([]byte(str)) {
+					return fmt.Errorf("please providde a valid node ID")
 				}
 				return nil
-			},
-		})
+			}).
+			Value(&configureArgs.Node),
+		)
 	}
-
-	if err := survey.Ask(questions, &configureArgs); err != nil {
-		if err == terminal.InterruptErr {
+	if err := huh.NewForm(huh.NewGroup(fields...)).Run(); err != nil {
+		if err == huh.ErrUserAborted {
 			return
 		}
 
