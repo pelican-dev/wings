fb04e57949e3d8c8908f79e3244eca4e3c529439
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2025-11-29 22:41:17 +0100

    Woppsie

--- environment/docker.go
+++ environment/docker.go
@@ -127,7 +127,7 @@ func createDockerNetwork(ctx context.Context, cli *client.Client) error {
 			"com.docker.network.bridge.enable_icc":           strconv.FormatBool(nw.EnableICC),
 			"com.docker.network.bridge.enable_ip_masquerade": "true",
 			"com.docker.network.bridge.host_binding_ipv4":    "0.0.0.0",
-			"com.docker.network.bridge.name":                 "featherpanel0",
+			"com.docker.network.bridge.name":                 "pelican0",
 			"com.docker.network.driver.mtu":                  strconv.FormatInt(nw.NetworkMTU, 10),
 		},
 	}
