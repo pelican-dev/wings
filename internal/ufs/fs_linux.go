7c11931d92948e1d941e347526d71d6326009dfb
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2026-08-03 10:35:22 +0200

    Add AT_SYMLINK_NOFOLLOW to utimesNanoAt call

--- internal/ufs/fs_linux.go
+++ internal/ufs/fs_linux.go
@@ -70,5 +70,5 @@ func (fs *UnixFS) Chtimesat(dirfd int, name string, atime, mtime time.Time) erro
 	set(1, mtime)
 
 	// This does support `AT_SYMLINK_NOFOLLOW` as well if needed.
-	return ensurePathError(unix.UtimesNanoAt(dirfd, name, utimes[0:], 0), "chtimes", name)
+	return ensurePathError(unix.UtimesNanoAt(dirfd, name, utimes[0:], AT_SYMLINK_NOFOLLOW), "chtimes", name)
 }
